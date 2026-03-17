cc57a6498d640e08f91a59ff470d2445d8144fba
Edwin Perez <edwinfrcperez@gmail.com>
2026-03-16 18:35:03 -0600

    add web routes

--- config/database.php
+++ config/database.php
@@ -86,14 +86,14 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'postgres.railway.internal'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
+            'database' => env('DB_DATABASE', 'railway'),
             'username' => env('DB_USERNAME', 'postgres'),
-            'password' => env('DB_PASSWORD', 'adminroot'),
-            'charset' => env('DB_CHARSET', 'utf8'),
-             'prefix' => '',
-    'schema' => 'public',
+            'password' => env('DB_PASSWORD', 'cRAzrwRGKskKHbcCqXGDibXDfgmLYzxh'),
+           // 'charset' => env('DB_CHARSET', 'utf8'),
+           //  'prefix' => '',
+    //'schema' => 'public',
         ],
 
         'sqlsrv' => [
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\Api\PostController as ApiPostController;
 
+// Inicio de Laravel
 Route::get('/', function () {
-    return "Laravel funcionando en Railway";
+    return view('index');
 });
+
+// CRUD de artículos (vista)
+Route::get('/articulos', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/list', [PostController::class, 'list'])->name('posts.list');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+// API REST (sin CSRF)
+Route::prefix('api')
+    ->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class])
+    ->group(function () {
+        Route::apiResource('posts', ApiPostController::class);
+    });
