15b3d48d8bbf96ff269c6846833a3e896fa4c5b2
Edwin Perez <edwinfrcperez@gmail.com>
2026-03-16 20:29:39 -0600

    nl

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -63,12 +63,17 @@
     </div>
 </div>
 
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script>
     const csrfToken = $('meta[name="csrf-token"]').attr('content');
     let modo = 'crear'; // o 'editar'
     $(function () {
 
-    
+
         let tabla = $('#tablaPosts').DataTable({
 paging:true,
 searching:true,
