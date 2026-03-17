bfb368363b224d0f350b38279c69ee454de7e6eb
Edwin Perez <edwinfrcperez@gmail.com>
2026-03-17 12:57:00 -0600

    hot

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -203,7 +203,7 @@ tabla.draw();
             modo = 'editar';
             const $tr = $(this).closest('tr');
             $('#postId').val($tr.find('td:nth-child(1)').text());
-            alert($('#postId').val());
+            //alert($('#postId').val());
             $('#title').val($tr.find('td:nth-child(2)').text());
             $('#content').val($tr.find('td:nth-child(3)').text());
             $('#tituloModal').text('Editar Post');
